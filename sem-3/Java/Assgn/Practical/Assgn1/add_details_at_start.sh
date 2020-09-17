e02820f95ac6bf06bf720b5b7818cc60174fb177
Ajinkya Rathod <57681532+ajinzrathod@users.noreply.github.com>
2020-09-17 22:40:27 +0530

    Edited Date in "add_details_at_start.sh"

--- sem-3/Java/Assgn/Practical/Assgn1/add_details_at_start.sh
+++ sem-3/Java/Assgn/Practical/Assgn1/add_details_at_start.sh
@@ -1,5 +1,5 @@
 for file in *.java
 do
-    sed -i  "1s/^/ \/* ===========================================================\n * \n * Roll No: 30\n * \n * File:      $file \n * Copyright: 21-Aug-2020 by Ajinkya Rathod(ajinzrathod)\n * \n * ========================================================== *\/  \n\n /" $file 
+    sed -i  "1s/^/ \/* ===========================================================\n * \n * Roll No: 30\n * \n * File:      $file \n * Copyright: <DATE> by Ajinkya Rathod(ajinzrathod)\n * \n * ========================================================== *\/  \n\n /" $file 
     echo "Added Initial Data in: $file"
 done
